8d7e4bf44a71e5eebee4748bfb07b0357a330175
Sam Leffler <sleffler@google.com>
2021-07-14 12:16:29 -0700

    Add a simple rust global allocator to each component.
    - add a linked_list_allocator::LockedHeap instance to each component that
      might want to allocate memory and init the allocator with a fixed-size
      memory block in the component post_init hook
    - add an alloc_test shell command that exercises the allocator
    
    This does not dynamically add memory or support sharing memory between
    components; this is an intermediate step to simplify bringing in crates
    that want to allocate memory (e.g. hashbrown)..
    
    Change-Id: Idaf11fb5d4999218c75bf932133df24de35e3053
    GitOrigin-RevId: 7c9b14bf9463239ce030c374b58a140f0835759e

--- apps/system/components/DebugConsole/kata-debug-console/src/run.rs
+++ apps/system/components/DebugConsole/kata-debug-console/src/run.rs
@@ -12,11 +12,29 @@
 // std:: requires at least an allocator, which Kata does not have yet. For now
 // the CLI will be implemented with only core::.
 #![no_std]
+// NB: not really what we want but resolves undefineds for now
+#![feature(default_alloc_error_handler)]
 
 extern crate panic_halt;
 
 use kata_shell;
 use kata_uart_client;
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static KATA_ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+#[no_mangle]
+// NB: use post_init insted of pre_init so syslog interface is setup
+pub extern "C" fn post_init() {
+    // TODO(sleffler): temp until we integrate with seL4
+    static mut HEAP_MEMORY: [u8; 16 * 1024] = [0; 16 * 1024];
+    unsafe {
+        KATA_ALLOCATOR
+            .lock()
+            .init(HEAP_MEMORY.as_mut_ptr() as usize, HEAP_MEMORY.len());
+    }
+}
 
 /// Entry point for DebugConsole. Runs the shell with UART IO.
 #[no_mangle]
