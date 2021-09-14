0db63cfb4ffb7b9d3976117c43165efb0f257bf7
Sam Leffler <sleffler@google.com>
2021-09-14 16:11:16 -0700

    Add SecurityCoordinator skeleton.
    - add SecurityCoordinator component (needs mailbox support, just
      a fake which should be enabled with a feature flag)
    - connect to ProcessManager & MlCoordinator - temproarily connect
      to DebugConsole to enable scecho test command
    - expand Bundle to hold application information (may need more elf)
    - connect ProcessManager::{install, uninstall} to SecurityCoordinator
      (no application binary yet, needs global page allocator)
    
    Notes:
    - SecurityCoordinator depends on camkes for thread synchronization
    - private heap is 8KB (and could possible be less; need to tune)
    - camkes interface connection uses seL4RPCOverMultiSharedData so ipc
      buffers are 4KB; the request & reply serde buffers are 2KB but could
      be near 4KB since they are used sequentially and the other params
      are a few bytes (but beware of camkes stack allocation)
    - the camkes SecurityCoordinator::request rpc is defined so that the
      request param has reasonable handling but the reply param requires
      a full copy (even if only partly used); haven't found a way to
      express the desired handling
    
    Change-Id: I686dc2d501e39bc8c27fe22db40657165a55b472
    GitOrigin-RevId: db1536c241e28ddda1dc8f8da341b8c667ed6646

---
+++ apps/system/components/SecurityCoordinator/kata-security-component/src/run.rs
@@ -0,0 +1,55 @@
+//! Kata OS Security Coordinator component support.
+
+// Code here binds the camkes component to the rust code.
+#![no_std]
+
+use core::slice;
+use kata_allocator;
+use kata_logger::KataLogger;
+extern crate kata_panic;
+use kata_security_common::*;
+use kata_security_coordinator::KATA_SECURITY;
+use log::trace;
+
+#[no_mangle]
+pub extern "C" fn pre_init() {
+    static KATA_LOGGER: KataLogger = KataLogger;
+    log::set_logger(&KATA_LOGGER).unwrap();
+    // NB: set to max; the LoggerInterface will filter
+    log::set_max_level(log::LevelFilter::Trace);
+
+    // TODO(sleffler): temp until we integrate with seL4
+    // TODO(sleffler): should be used rarely
+    static mut HEAP_MEMORY: [u8; 8 * 1024] = [0; 8 * 1024];
+    unsafe {
+        kata_allocator::ALLOCATOR.init(HEAP_MEMORY.as_mut_ptr() as usize, HEAP_MEMORY.len());
+        trace!(
+            "setup heap: start_addr {:p} size {}",
+            HEAP_MEMORY.as_ptr(),
+            HEAP_MEMORY.len()
+        );
+    }
+
+    // Complete KATA_SECURITY setup. This is as early as we can do it given that
+    // it needs the GlobalAllocator.
+    unsafe {
+        KATA_SECURITY.init();
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn security_request(
+    c_request: SecurityRequest,
+    c_request_buffer_len: u32,
+    c_request_buffer: *const u8,
+    c_reply_buffer: *mut SecurityReplyData,
+) -> SecurityRequestError {
+    unsafe {
+        KATA_SECURITY.request(
+            c_request,
+            slice::from_raw_parts(c_request_buffer, c_request_buffer_len as usize),
+            &mut (*c_reply_buffer)[..],
+        )
+    }
+    .map_or_else(|e| e, |_v| SecurityRequestError::SreSuccess)
+}
